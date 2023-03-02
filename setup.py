4e7a384a8f5e5fd6bbb2384d024cf007cd11939c
Oliver Kurth <okurth@gmail.com>
2023-03-01 17:21:29 -0800

    move to pyproject.toml
    Change-Id: I36afe37373ccc1c22826131ccb194094a37a0125

--- setup.py
+++ setup.py
@@ -3,28 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
 #
 
-import os
-from version import get_installer_version
-from setuptools import setup, find_packages
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "requirements.txt")) as requirements_txt:
-    REQUIRES = requirements_txt.read().splitlines()
-
-setup(
-    name='photon-installer',
-    description='Installer code for photon',
-    packages=find_packages(include=['photon_installer', 'photon_installer.modules']),
-    install_requires=REQUIRES,
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-            'photon-installer = photon_installer.main:main',
-            'photon-iso-builder = photon_installer.isoBuilder:main'
-        ]
-    },
-    version='2.1+'+get_installer_version(),
-    author_email='gpiyush@vmware.com'
-)
+setup()
